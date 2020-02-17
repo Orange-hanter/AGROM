d3d48ab1542dc2a6e999c3e0ace4915df1de7c3a
Daniil Akhramiuk <Danil.bel53@gmail.com>
2020-02-17 16:17:09 +0300

    Implemented median filter

--- RingBuffer.hpp
+++ RingBuffer.hpp
@@ -46,6 +46,30 @@ public:
     return (data[0] + data[1] + data[2] + data[3] + data[4]) / 5.f;
   };
 
+  T getMid()
+  {
+    T newData[5]= {data[0] ,data[1], data[2],  data[3], data[4]};
+    
+    for (size_t i = 0; i < 5; i++)
+    {
+      size_t buf = i;
+      for (size_t j = i; j < 5; j++)
+      {
+        if( data[buf] > data[j] )
+          buf = j;
+      }
+      if(buf != i)
+      {
+        T tmp = data[i];
+        data[i] = data[buf];
+        data[buf] = tmp;
+      }
+      
+    }
+    
+    return newData[2];
+  };
+
   void check()
   {
     bool tooLong = (micros() - this->prevValue) > 5000000; //TODO confirm that this value (time between signals) not to big or small
@@ -60,7 +84,7 @@ class RPMMetr : public RingBuffer<long>
 public:
   float getRPM( int COUNT_OF_BLADES = 1)
   {
-    long calc = (this->getAverage() * COUNT_OF_BLADES);
+    long calc = (this->getMid() * COUNT_OF_BLADES);
     return calc == 0 ? 0 : (60000000.f / calc); //TODO aproof math of calculation RPM
   }
 };
